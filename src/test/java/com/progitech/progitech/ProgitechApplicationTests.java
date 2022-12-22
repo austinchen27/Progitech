bcb8cb6938caacb88831d457abbf08cf779fde2a
Austin Chen <114212282+austinchen27@users.noreply.github.com>
2022-12-22 15:21:58 -0800

    first kermit

---
+++ src/test/java/com/progitech/progitech/ProgitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progitech.progitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
